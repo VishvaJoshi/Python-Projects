f1a049879e80d0413d9039469455ca9738782770
Vishva Joshi <166016227+VishvaJoshi@users.noreply.github.com>
2024-05-30 22:57:54 +0100

    Add files via upload

---
+++ Fizz Buzz.py
@@ -0,0 +1,9 @@
+for num in range (1,101):
+   if num % 15 == 0:
+     print("FIzz Buzz")
+   elif num % 3 == 0:
+    print("FIzz")
+   elif num % 5 == 0:
+    print("Buzz")
+   else:
+    print(num)
